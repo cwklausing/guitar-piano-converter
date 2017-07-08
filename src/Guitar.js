35fb786cc80c4a4dd29de4168fb6c87a1e8ac6b0
cwklausing <cwklausing@gmail.com>
2017-07-08 13:37:48 -0500

    add string images/styling to guitar

--- src/Guitar.js
+++ src/Guitar.js
@@ -1,22 +1,15 @@
 import React from 'react';
 
 class Guitar extends React.Component {
-	renderRow(strings) {
-		return (
-			<div className="string-row">
-				<div className="fret" />
-			</div>
-		);
-	}
-
 	render() {
 		const guitar = this.props.guitar;
 		const strings = guitar.map((string, index) => {
 			const frets = string.frets.map((value, index) => {
-				return <div className={'fret-' + index} key={index} />;
+				return <div className={'fret fret-' + index} key={index} />;
 			});
 			return (
-				<div className={'string-' + index} key={index}>
+				<div className={'string string-' + index} key={index}>
+					<div className="string-image" />
 					{frets}
 				</div>
 			);
