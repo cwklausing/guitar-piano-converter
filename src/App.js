be5fb389a247a2952b57d6004f8d3d3dbb9c7095
cwklausing <cwklausing@gmail.com>
2017-07-08 12:47:45 -0500

    add guitar state to app, some guitar styling

--- src/App.js
+++ src/App.js
@@ -5,12 +5,28 @@ import Piano from './Piano';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		const fretNumber = 10;
+
+		super();
+		this.state = {
+			guitar: {
+				E1: Array(fretNumber).fill(null),
+				A: Array(fretNumber).fill(null),
+				D: Array(fretNumber).fill(null),
+				G: Array(fretNumber).fill(null),
+				B: Array(fretNumber).fill(null),
+				E2: Array(fretNumber).fill(null)
+			}
+		};
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<Header />
 				<main className="app-main">
-					<Guitar />
+					<Guitar guitar={this.state.guitar} />
 					<Piano />
 				</main>
 			</div>
--- src/Guitar.js
+++ src/Guitar.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
-function Guitar(props) {
-	return (
-		<div className="guitar">
-			<div className="guitar-neck">
-				<div className="guitar-string" />
-				<div className="guitar-string" />
-				<div className="guitar-string" />
-				<div className="guitar-string" />
-				<div className="guitar-string" />
-				<div className="guitar-string" />
+class Guitar extends React.Component {
+	renderRow(strings) {
+		return (
+			<div className="string-row">
+				<div className="fret" />
 			</div>
-		</div>
-	);
+		);
+	}
+
+	render() {
+		const strings = this.props.guitar;
+		console.log(strings);
+		return (
+			<div className="guitar">
+				<div className="guitar-neck">
+					{}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Guitar;
