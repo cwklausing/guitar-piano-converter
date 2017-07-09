dbace34aa8eaf5d7654624a5ecd9337dc9a1e101
cwklausing <cwklausing@gmail.com>
2017-07-09 13:07:11 -0500

    refactor guitar to functional component

--- src/Guitar.js
+++ src/Guitar.js
@@ -1,32 +1,30 @@
 import React from 'react';
 
-class Guitar extends React.Component {
-	render() {
-		const notes = this.props.notes;
-		const strings = notes.map((row, index) => {
-			const frets = row.map((value, index) => {
-				return <div className={'fret fret-' + index} key={index} />;
-			});
-			return (
-				<div className={'string string-' + index} key={index}>
-					{frets}
-				</div>
-			);
-		});
-		const frets = this.props.fretImages.map((value, index) => {
-			return <div className={'fret-image fret-image-' + index} key={index} />;
+function Guitar(props) {
+	const notes = props.notes;
+	const strings = notes.map((row, index) => {
+		const frets = row.map((value, index) => {
+			return <div className={'fret fret-' + index} key={index} />;
 		});
 		return (
-			<div className="guitar">
-				<div className="fret-images">
-					{frets}
-				</div>
-				<div className="guitar-neck">
-					{strings}
-				</div>
+			<div className={'string string-' + index} key={index}>
+				{frets}
 			</div>
 		);
-	}
+	});
+	const frets = props.fretImages.map((value, index) => {
+		return <div className={'fret-image fret-image-' + index} key={index} />;
+	});
+	return (
+		<div className="guitar">
+			<div className="fret-images">
+				{frets}
+			</div>
+			<div className="guitar-neck">
+				{strings}
+			</div>
+		</div>
+	);
 }
 
 export default Guitar;
