11594e1013f8f6338491300b7e5e5b46e2e1516d
cwklausing <cwklausing@gmail.com>
2017-07-10 10:55:05 -0500

    add logic to account for repeat B note on guitar

--- src/Piano.js
+++ src/Piano.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Piano(props) {
 	const notes = props.notes;
-	const whiteKeys = [0, 1, 3, 5, 7, 8, 10, 12, 13, 15, 17, 19, 20, 22, 24, 25, 27, 29, 31];
+	const whiteKeys = [0, 1, 3, 5, 7, 8, 10, 12, 13, 15, 17, 19, 20, 21, 23, 25, 26, 28, 30, 32];
 	const findKeyColor = keyNumber => {
 		for (let i = 0; i < whiteKeys.length; i++) {
 			if (whiteKeys[i] === keyNumber) {
@@ -14,15 +14,21 @@ function Piano(props) {
 	const keys = notes.map((section, sectionIndex) => {
 		const sectionOffset = sectionIndex * 5;
 		const keySection = section.map((value, keyIndex) => {
+			const currentNote = notes[sectionIndex][keyIndex];
 			const keyNumber = keyIndex + sectionOffset;
-			return (
-				<div
-					className={
-						'key key-num-' + keyNumber + ' key-' + findKeyColor(keyNumber) + ' key-section-' + sectionIndex
-					}
-					key={keyIndex}
-				/>
-			);
+			var keyClass =
+				'key key-num-' + keyNumber + ' key-' + findKeyColor(keyNumber) + ' key-section-' + sectionIndex;
+
+			if (currentNote === 'active') {
+				keyClass += ' active';
+			} else if ((sectionIndex === 3 && keyIndex === 4) || (sectionIndex === 4 && keyIndex === 0)) {
+				// Account for repeat B note on string 3 & 4
+				if (notes[3][4] === 'active' || notes[4][0] === 'active') {
+					keyClass += ' active';
+				}
+			}
+
+			return <div className={keyClass} key={keyIndex} />;
 		});
 		return keySection;
 	});
